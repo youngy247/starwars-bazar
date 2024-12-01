2f44d866e5caedf2264555124b0115c293e44ed0
Adam <adamyoungy678@gmail.com>
2024-12-01 21:59:19 +0000

    feat: wrap BazarPage with ErrorBoundaryWrapper for enhanced error handling

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
+import ErrorBoundaryWrapper from '@/components/ErrorBoundaryWrapper/ErrorBoundaryWrapper';
 import React from 'react';
 import BazarPage from './home/page';
 
 export default function Page() {
-  return <BazarPage />;
+  return (
+    <ErrorBoundaryWrapper>
+      <BazarPage />
+    </ErrorBoundaryWrapper>
+  );
 }
